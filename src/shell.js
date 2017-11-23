7b07b151670950eb2dfc8d64b3efda843a5b0885
Sung Kim <sungmkim80@gmail.com>
2017-11-22 22:57:52 -0500

    Turned Google Book Shell into a CLI

--- src/shell.js
+++ src/shell.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+// https://developer.atlassian.com/blog/2015/11/scripting-with-node/
 const GoogleBookSearchShell = require('./GoogleBookSearchShell');
 
 const shell = new GoogleBookSearchShell();
